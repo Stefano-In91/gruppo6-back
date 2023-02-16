1a4bde051a4fde054c5021a311806e9a63c5abbd
Alex100688 <alessiozingaro2@gmail.com>
2023-02-16 09:45:06 +0100

    Creato il seeder

--- database/seeders/ArtistSeeder.php
+++ database/seeders/ArtistSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Artist;
+use Faker\Generator as Faker; 
+use Illuminate\Support\Str;
 class ArtistSeeder extends Seeder
 {
     /**
@@ -12,8 +14,14 @@ class ArtistSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 20; $i++) { 
+            $new_artist = new Artist();
+            $new_artist->artist_nickname = $faker->name(30);
+            $new_artist->introduction_text = $faker->text(100);
+            $new_artist->slug = Str::slug($new_artist->artist_nickname);
+            $new_artist->save();
+        }
     }
 }
