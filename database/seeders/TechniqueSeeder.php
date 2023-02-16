505d785601ed55731713124a5ebbcacb5cba7856
Alex100688 <alessiozingaro2@gmail.com>
2023-02-16 12:38:11 +0100

    creato il seeder technique

--- database/seeders/TechniqueSeeder.php
+++ database/seeders/TechniqueSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technique;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rules\Unique;
 
 class TechniqueSeeder extends Seeder
 {
@@ -12,8 +16,14 @@ class TechniqueSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 20 ; $i++) { 
+            $new_technique = new Technique();
+            $new_technique->name = $faker->name(30);
+            $new_technique->description = $faker->text(1000);
+            $new_technique->slug = Str::slug($new_technique -> name);
+            $new_technique->save();
+        }
     }
 }
