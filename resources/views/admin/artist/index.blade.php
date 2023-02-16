a8d6fc3d885f55a200085384746ba45fb152ecfa
dav100ide <oreglio.dev@gmail.com>
2023-02-16 17:50:21 +0100

    add 3puntini se introduction_text è troppoLungo

--- resources/views/admin/artist/index.blade.php
+++ resources/views/admin/artist/index.blade.php
@@ -10,36 +10,81 @@ $average_review = 4;
 @if (session('message'))
 <div class="alert alert-success">{{ session('message') }}</div>
 @endif
-<div class="details">
+<div class="container-fluid">
 	<h1>Dettagli Artista</h1>
-	<div class="artist">
-		{{-- foto profilo --}}
-		<div class="artist__avatar">
-		@if ($artist->profile_photo)
-		<img src="{{asset("storage/$artist->profile_photo")}}" alt="{{$artist->artist_nickname}}" >
-		@else
-		<img src="https://via.placeholder.com/100" alt="placeholder" >
-		@endif
-		</div>
-		{{-- /foto profilo --}}
+	<div class="details | row">
+		<div class="artist | p-3 rounded-3 col-12 col-xxl-6">
+			{{-- foto profilo --}}
+			<div class="artist__avatar">
+				@if ($artist->profile_photo)
+				<img src="{{asset("storage/$artist->profile_photo")}}" alt="{{$artist->artist_nickname}}" >
+				@else
+				<img src="https://via.placeholder.com/100" alt="placeholder" >
+				@endif
+			</div>
+			{{-- /foto profilo --}}
+	
+			<div class="artist__info">
+				<h3>{{$artist->artist_nickname}}</h3>
+				{{-- descrizione intro artista --}}
+				<div id="introduction_text" >{{$artist->introduction_text}} Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia ipsum repudiandae saepe alias facilis sint dignissimos aliquam accusamus. Earum qui non, dignissimos sed delectus incidunt harum eveniet? Sed, optio laborum! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque mollitia nostrum ut iusto! Temporibus autem ratione error unde in quae deleniti nobis aperiam repellendus dolorem voluptates, quam quos facilis id. </div>				
+				<script>	
+					const text = document.getElementById('introduction_text');
+					const textLength = 100; //mostra solo i primi 100 caratteri
+					let isClicked = false;
+					const textInnerhtml = text.textContent;
 
-		<div class="artist__info">
-			<h3>{{$artist->artist_nickname}}</h3>
-			<p>{{$artist->introduction_text}}</p>
-			<div class="artist__review">
-				{{-- stelline  --}}
-				<div class="artist__review__stars">
-					@for ($i = 0; $i < $average_review ; $i++)
-					<i class="fa-solid fa-star"></i>
-					@endfor
-					@for ($i = 0; $i < 5 - $average_review ; $i++)
-					<i class="fa-regular fa-star"></i>
-					@endfor
+					if (text.textContent.length > textLength ) {
+						text.style.cursor = "pointer";
+						text.style.whiteSpace = "nowrap";
+						text.style.overflow = "hidden";
+						text.style.textOverflow = "ellipsis";
+
+						text.addEventListener("click", function() {
+						isClicked = !isClicked;
+							if (isClicked) {
+									text.style.whiteSpace = "normal";
+									text.style.overflow = "visible";
+									text.style.textOverflow = "clip";
+									text.textContent =  `${textInnerhtml} Nascondi`;
+							} else {
+									text.style.whiteSpace = "nowrap";
+									text.style.overflow = "hidden";
+									text.style.textOverflow = "ellipsis";
+									text.textContent = text.textContent.slice(0, textLength);
+							}
+    				});
+				}
+				</script>
+				{{-- /descrizione intro artista --}}
+				<div class="artist__contacts">
+					<ul>
+						<li>
+							<i class="fa-solid fa-location-dot"></i> {{$artist->address}}
+						</li>
+						<li>
+							<i class="fa-solid fa-phone"></i> {{$artist->phone_number}}
+						</li>
+					</ul>
 				</div>
-				{{-- /stelline --}}
-				<a href="#">Vedi tutte le recensioni</a>
+	
+				<div class="artist__review | mt-auto">
+					{{-- stelline  --}}
+					<div class="artist__review__stars">
+						Valutazione:
+						@for ($i = 0; $i < $average_review; $i++)
+						<i class="fa-solid fa-star"></i>
+						@endfor
+						@for ($i = 0; $i < 5 - $average_review; $i++)
+						<i class="fa-regular fa-star"></i>
+						@endfor
+					</div>
+					{{-- /stelline --}}
+					<a href="#">Vedi tutte le recensioni</a>
+				</div>
 			</div>
 		</div>
+		
 	</div>
 </div>
   
