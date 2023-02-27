bbce7bf6311b718b6967d25880cee9777c49a549
dav100ide <oreglio.dev@gmail.com>
2023-02-27 12:37:37 +0100

    sovrascrivo var primary-color di btn di Bootstrap

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-  <div class="container">
+  <div class="container d-none">
     <div class="py-4">
       <h2>{{ $message->title }}</h2>
       <h4>{{ $message->sender_email }}</h4>
@@ -11,4 +11,12 @@
     <a href="{{ route('admin.messages.index') }}" class="btn btn-primary">Torna a lista
       Messaggi</a>
   </div>
+  {{-- card message --}}
+  <div>
+
+  </div>
+  {{-- /card message --}}
+  <a href="{{ route('admin.messages.index') }}" class="btn btn-primary">
+    Torna a lista Messaggi
+  </a>
 @endsection
