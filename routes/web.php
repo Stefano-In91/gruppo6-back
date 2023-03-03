3e2d8bec983f55db69eed7366d59b3d4c9c805b5
Stefano.In91 <ste.ince91@gmail.com>
2023-03-03 12:28:00 +0100

    adds stats to dashboard

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Artist;
+use Illuminate\Support\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,7 +31,31 @@ Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () 
     Route::get('/dashboard', function () {
         $user = Auth::user();
         if ( Artist::firstWhere('user_id', $user->id )) {
-            return view('admin.dashboard', compact('user'));
+
+            $artist = Artist::firstWhere('user_id', $user->id );
+            $artist->load('messages', 'ratings', 'reviews');
+
+            $messages = $artist->messages;
+            $ratings = $artist->ratings;
+            $reviews = $artist->reviews;
+            $date = Carbon::now();
+            // trovo numero messaggi ricevuti nell'ultimo mese
+            $filtered_messages = $messages->filter(function($message) use ($date) {
+                return strtotime($message->date) >= strtoTime($date->subMonth());
+            });
+            $messages = $filtered_messages->count();
+            // trovo valutazioni ricevute nell'ultimo mese
+            $filtered_ratings = $ratings->filter(function($rating) use ($date) {
+                return strtotime($rating->pivot->rating_date) >= strtoTime($date->subMonth());
+            });
+            $ratings = $filtered_ratings->count(); 
+            // trovo recensioni ricevute nell'ultimo mese
+            $filtered_reviews = $reviews->filter(function($review) use ($date) {
+                return strtotime($review->date) >= strtoTime($date->subMonth());
+            });
+            $reviews = $filtered_reviews->count();
+
+            return view('admin.dashboard', compact('user', 'messages', 'ratings', 'reviews'));
         } else {
             return redirect()->route('admin.artists.create')->with('message', "Crea il tuo profilo Artista per continuare");
         }
